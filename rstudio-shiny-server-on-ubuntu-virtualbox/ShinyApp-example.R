75995efb0902d758ffc4bf72dd24de0f2f6311d2
MarkRijckenberg <markrijckenberg@gmail.com>
2015-05-30 13:06:00 +0200

    Update ShinyApp-example.R

--- rstudio-shiny-server-on-ubuntu-virtualbox/ShinyApp-example.R
+++ rstudio-shiny-server-on-ubuntu-virtualbox/ShinyApp-example.R
@@ -1,3 +1,4 @@
+# https://github.com/rstudio/shiny-examples
 install.packages("mclust")
 if (!require('shiny')) install.packages("shiny")
 shiny::runGitHub("shiny-examples", "rstudio", subdir = "040-dynamic-clustering")
